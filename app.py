e5af6ae448f0c6e26935acf4db1065cc61f6456e
Nanya-000 <ngwutaifunanyarosemary@gmail.com>
2025-12-26 18:28:38 +0100

    Update app.py
    Add basic order endpoints

--- app.py
+++ app.py
@@ -10,3 +10,19 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+orders = []
+
+@app.route("/orders", methods=["GET"])
+def get_orders():
+    return jsonify(orders)
+
+@app.route("/orders", methods=["POST"])
+def create_order():
+    order = {
+        "id": len(orders) + 1,
+        "item": "Crochet Bag",
+        "status": "pending"
+    }
+    orders.append(order)
+    return jsonify(order), 201
